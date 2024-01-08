7fbb85d61cf006ec299a30bb6c9358d4d3f9326c
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-01-08 19:49:17 +0100

    Treball fet dilluns 8-1-2024

--- Projecte Arrays/js/script.js
+++ Projecte Arrays/js/script.js
@@ -13,6 +13,9 @@ let movies = [];
 // Polar Chart
 let myChart;
 
+let iniciado = false; // Booleano que indica si la tabla se ha iniciado o no
+let orderAsc; // Booleano que indica si la tabla se ordena ascendente o descendente
+
 // POKEMONS
 fetch("js/data/pokemon.json")
 .then((response) => response.json())
@@ -25,6 +28,7 @@ fetch("js/data/pokemon.json")
 		dadesPokemon[1] = dada.img; // imatge
 		dadesPokemon[2] = dada.num; // numero
 		dadesPokemon[3] = (dada.weight).substring(0, (dada.weight).length-3); // pes (solo número)
+		dadesPokemon[4] = dada.type;
 
 		pokemons.push(dadesPokemon);
 	});
@@ -96,7 +100,7 @@ fetch("js/data/movies.json")
 /* Función que crea la tabla principal 
    (pos 0 = pokemons, pos 1 = municipis, pos 2 = meteorits, pos 3 = movies) */
 function creaTaula() {
-	console.clear()
+	//console.clear()
 	for (let i = 0; i < 1000; i++) {
 		let dades = [];
 		dades[0] = pokemons[i],
@@ -106,7 +110,7 @@ function creaTaula() {
 
 		taula[i] = dades;
 	};
-	console.table(taula);
+	//console.table(taula);
 	printList(getDades()); // Printea la tabla en pantalla
 }
 
@@ -117,36 +121,33 @@ function refresh() {
 
 /* Función que ordena la lista principal.
    Recibe por parámetros si el órden es ascendente o descendente */
-function orderList(orden) {
-	console.clear();
-	// Ordenamos alfabéticamente
-	pokemons.sort();
-	municipis.sort();
-	meteorits.sort();
-	movies.sort();
-
-	if (orden == 'asc') { // Volteamos si el orden es ascendente
-		pokemons.reverse();
-		municipis.reverse();
-		meteorits.reverse();
-		movies.reverse();
-	}
+function orderList(index) {
+	//console.clear();
+	if (orderAsc == true) { getDades().sort(function(a, b) {return a[0]<b[0]}) ;orderAsc = false; }
+	else { getDades().reverse(function(a, b) {return a[0]<b[0]}) ;orderAsc = false; }
 	creaTaula(); // Vuelve a crear la tabla ordenada
 }
 
-/* Función que recibe un texto por prompt y devuelve por pantalla 
-   los elementos de la lista que contengan el texto */
-function searchList() {
-	let text = prompt("Introdueix text");
-	let dades = [];
-	// Foreach que comprueba si el elemento de la list contiene el texto del prompt
-	getDades().forEach(dada => {
-		if (dada[0].toLowerCase().includes(text.toLowerCase())) {
-			dades.push(dada);
-		}
-	});
-	if (dades.length==0) return alert('Not found!'); // Si no ha encontrado nada muestra un alert
-	printList(dades); // Printea la tabla en pantalla
+/* Event Listener que detecta cada vez que escrivimos en la barra de búsqueda 
+   y llama a searchList() */
+let inputSearch = document.getElementById('txtSearch')
+inputSearch.addEventListener('input', (e) => {
+	searchList(inputSearch.value)
+});
+
+/* Función que recibe el valor del input HTML y devuelve por pantalla 
+   los elementos de la lista que contengan ese texto */
+function searchList(inputSearch) {
+	if (iniciado) {
+		let dades = [];
+		// Foreach que comprueba si el elemento de la list contiene el texto del input HTML
+		getDades().forEach(dada => {
+			if (dada[0].toLowerCase().includes(inputSearch.toLowerCase())) {
+				dades.push(dada);
+			}
+		});
+		printList(dades); // Printea la tabla en pantalla
+	}
 }
 
 /* Función que calcula la media del elemento de la lista, y la muestra por pantalla */
@@ -173,7 +174,9 @@ function getRadioButton() {
 	if (document.getElementById("pokemons").checked) return document.getElementById('pokemons').value;
 	if (document.getElementById("municipis").checked) return document.getElementById('municipis').value;
 	if (document.getElementById("meteorits").checked) return document.getElementById('meteorits').value;
-	if (document.getElementById("movies").checked) return document.getElementById('movies').value;	
+	if (document.getElementById("movies").checked) return document.getElementById('movies').value;
+	
+	return null;
 }
 
 /* Función que devuelve la lista de datos según el Radio button chequeado */
@@ -190,64 +193,68 @@ function getDades() {
 /* Función que muestra por pantalla los datos de la lista */
 function printList(dades) {
 	let tipusDada = getRadioButton(); // Cogemos el radio button chequeado
+	if (getRadioButton() == null) return; // Si no hay ningún radio button chequeado no hacemos nada
 	let divTable = document.getElementById('resultat');
 	// table
 	let table = '<table>';
 	
-	// Switch para mostrar los datos de la lista 
-	switch (tipusDada) {
-		case 'pokemons': // POKEMONS
-			// th
-			table += '<tr><th>#</th><th>Imatge</th><th>Nom</th><th>Pes</th></tr>';
-			dades.forEach(dada => {
-				// tr
-				table += '<tr>';
-				// td
-				table += `<td>${dada[2]}</td><td><img src='${dada[1]}'></td><td>${dada[0]}</td><td>${dada[3]}</td>`;
-				table += '</tr>';
-			});
-		break;
-	
-		case 'municipis': // MUNICIPIS 
-			// th
-			table += '<tr><th>Nom</th><th>Escut</th><th>INE</th><th>Habitants</th></tr>';
-			dades.forEach(dada => {
-				// tr
-				table += '<tr>';
-				// td
-				table += `<td>${dada[0]}</td><td><img src='${dada[1]}'></td><td>${dada[2]}</td><td>${dada[3]}</td>`;
-				table += '</tr>';
-			});
-		break;
+	if (dades.length != 0) {
+		// Switch para mostrar los datos de la lista 
+		switch (tipusDada) {
+			case 'pokemons': // POKEMONS
+				// th
+				table += '<tr><th onclick="orderList(2)">#</th><th>Imatge</th><th onclick="orderList(0)">Nom</th><th onclick="orderList(3)">Pes</th></tr>';
+				dades.forEach(dada => {
+					// tr
+					table += '<tr>';
+					// td
+					table += `<td>${dada[2]}</td><td><img src='${dada[1]}'></td><td>${dada[0]}</td><td>${dada[3]}</td>`;
+					table += '</tr>';
+				});
+			break;
+		
+			case 'municipis': // MUNICIPIS 
+				// th
+				table += '<tr><th>Nom</th><th>Escut</th><th>INE</th><th>Habitants</th></tr>';
+				dades.forEach(dada => {
+					// tr
+					table += '<tr>';
+					// td
+					table += `<td>${dada[0]}</td><td><img src='${dada[1]}'></td><td>${dada[2]}</td><td>${dada[3]}</td>`;
+					table += '</tr>';
+				});
+			break;
 
-		case 'meteorits': // METEORITS
-			// th
-			table += '<tr><th>Nom</th><th>#</th><th>Any</th><th>Masa</th></tr>';
-			dades.forEach(dada => {
-				// tr
-				table += '<tr>';
-				// td
-				table += `<td>${dada[0]}</td><td>${dada[1]}</td><td>${dada[2]}</td><td>${dada[3]}</td>`;
-				table += '</tr>';
-			});
-		break;
-
-		case 'movies': // MOVIES
-			// th
-			table += '<tr><th>Title</th><th>Genres</th><th>Any</th><th>Rating</th></tr>';
-			dades.forEach(dada => {
-				// tr
-				table += '<tr>';
-				// td
-				table += `<td>${dada[0]}</td><td>${dada[1]}</td><td>${dada[2]}</td><td>${dada[3]}</td>`;
-				table += '</tr>';
-			});
-		break;
+			case 'meteorits': // METEORITS
+				// th
+				table += '<tr><th>Nom</th><th>#</th><th>Year</th><th>Masa</th></tr>';
+				dades.forEach(dada => {
+					// tr
+					table += '<tr>';
+					// td
+					table += `<td>${dada[0]}</td><td>${dada[1]}</td><td>${dada[2]}</td><td>${dada[3]}</td>`;
+					table += '</tr>';
+				});
+			break;
+			
+			case 'movies': // MOVIES
+				// th
+				table += '<tr><th>Title</th><th>Genres</th><th>Year</th><th>Rating</th></tr>';
+				dades.forEach(dada => {
+					// tr
+					table += '<tr>';
+					// td
+					table += `<td>${dada[0]}</td><td>${dada[1]}</td><td>${dada[2]}</td><td>${dada[3]}</td>`;
+					table += '</tr>';
+				});
+			break;
+		}
 	}
 
 	table += '</table>';
 	divTable.innerHTML = table;
-
+	iniciado = true;
+	
 	// Creamos el Polar Chart
 	creaChart(tipusDada);
 }
@@ -268,23 +275,21 @@ function creaChart(tipusDada) {
 		// POKEMONS
 		arrayLabels = ["Grass", "Poison", "Fire", "Flying", "Water", "Bug", "Normal", "Electric", "Ground", "Fighting", "Psychic", "Rock", "Ice", "Ghost", "Dragon"];
 		arrayLabelsGraf = [14, 33, 12, 19, 32, 12, 24, 9, 14, 8, 14, 11, 5, 3, 3];
-		for (let i = 0; i < arrayLabels.length; i++) {
-			borderColor[i] = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-		}
-		for (let i = 0; i < borderColor.length; i++) {
-			backgroundColor[i] = `${borderColor[i].substring(0, borderColor[i].length - 1)}, 0.2)`;
-		}
 	} else if (tipusDada == 'movies') {
 		// MOVIES
 		arrayLabels = ["Drama", "Crime", "Action", "Thriller", "Biography", "History", "Adventure", "Fantasy", "Western", "Romance", "Sci-Fi", "Mystery", "Comedy", "War", "Family", "Animation", "Musical", "Music", "Horror", "Film-Noir", "Sport"];
 		arrayLabelsGraf = [185, 53, 39, 60, 27, 15, 57, 28, 8, 27, 32, 33, 44, 28, 25, 22, 5, 8, 4, 6, 10];
-		for (let i = 0; i < arrayLabels.length; i++) {
-			borderColor[i] = `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-		}
-		for (let i = 0; i < borderColor.length; i++) {
-			backgroundColor[i] = `${borderColor[i].substring(0, borderColor[i].length - 1)}, 0.2)`;
-		}
 	} else return;
+
+	// For que por cada label asigna un color rgb aleatorio al array borderColor
+	arrayLabels.forEach(label => {
+		borderColor.push(`rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
+	});
+
+	// For que por cada borderColor asigna el mismo color con una opacidad del 0.2 en el array backgroundColor
+	borderColor.forEach(color => {
+		backgroundColor.push(`${color.substring(0, color.length - 1)}, 0.2)`);
+	});
 
 	// Variable con los datos del Polar Chart
 	const data = {
